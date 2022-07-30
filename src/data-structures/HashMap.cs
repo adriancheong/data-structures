275c884ca792cbc8d450e1ffe47fb25cd3267a2a
Adrian Cheong <addy_cheong@yahoo.com.sg>
2022-07-30 16:38:05 +0800

    added some test cases and some implementation

--- src/data-structures/HashMap.cs
+++ src/data-structures/HashMap.cs
@@ -1,16 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 namespace data_structures
 {
     public class HashMap<K,V>
     {
+        V[] entries = new V[4];
+        int entryCount = 0;
         public HashMap()
         {
         }
 
         public void put(K key, V value)
         {
-            throw new NotImplementedException();
+            if (entryCount >= entries.Length / 2)
+                resize();
+            int hashCode = key.GetHashCode() & 0x7fffffff % entries.Length;
+            entries[hashCode] = value;
+            entryCount++;
+        }
+
+        private void resize()
+        {
+            V[] newEntries = new V[entries.Length * 2];
+            Array.Copy(entries, newEntries, entries.Length);
+            entries = newEntries;
+        }
+
+        public V get(K key)
+        {
+            int hashCode = key.GetHashCode() & 0x7fffffff % entries.Length;
+            return entries[hashCode];
+        }
+
+        public int size()
+        {
+            return entries.Length;
         }
     }
 }
--- test/data-structures-test/TestHashMap.cs
+++ test/data-structures-test/TestHashMap.cs
@@ -11,5 +11,54 @@ namespace data_structures
             HashMap<string, int> hm = new HashMap<string, int>();
             hm.put("one", 1);
         }
+
+        [Fact]
+        public void HashMapCanGetAfterPutting()
+        {
+            int expected = 1;
+            HashMap<string, int> hm = new HashMap<string, int>();
+            hm.put("one", expected);
+
+            int actual = hm.get("one");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void HashMapCanGrowAutomatically()
+        {
+            // Given
+            HashMap<string, int> hm = new HashMap<string, int>();
+            hm.put("one", 1);
+            hm.put("two", 2);
+            Assert.Equal(4, hm.size());
+            hm.put("three", 3);
+            Assert.Equal(8, hm.size());
+            hm.put("four", 4);
+            Assert.Equal(8, hm.size());
+            hm.put("five", 5);
+            Assert.Equal(16, hm.size());
+
+        }
+
+        [Fact]
+        public void PutFiveItemsAndGetThemAll()
+        {
+            // Given
+            HashMap<string, int> hm = new HashMap<string, int>();
+            hm.put("one", 1);
+            hm.put("two", 2);
+            hm.put("three", 3);
+            hm.put("four", 4);
+            hm.put("five", 5);
+
+            Assert.Equal(1, hm.get("one"));
+            Assert.Equal(2, hm.get("two"));
+            Assert.Equal(3, hm.get("three"));
+            Assert.Equal(4, hm.get("four"));
+            Assert.Equal(5, hm.get("five"));
+        }
+
+
     }
 }
