141f1037ddf4fb5a2b95714bef3370b8492c214e
Adrian Cheong <adriancheong@gic.com.sg>
2022-07-31 13:25:17 +0800

    Added basic test cases and basic logic

--- src/data-structures/LinkedList.cs
+++ src/data-structures/LinkedList.cs
@@ -4,6 +4,47 @@ namespace data_structures
 {
     public class LinkedList<T>
     {
-        
+        Node<T> firstItem;
+        class Node<T>
+        {
+            public Node(T item)
+            {
+                this.Item = item;
+            }
+            public T Item { get; set; }
+            public Node<T> Next { get; set; }
+        }
+
+        public void AddFirst(T item)
+        {
+            if (firstItem == null)
+                firstItem = new Node<T>(item);
+            else
+            {
+                var newNode = new Node<T>(item);
+                newNode.Next = firstItem;
+                firstItem = newNode;
+            }
+
+
+        }
+
+        public int count()
+        {
+            int count = 0;
+            var iter = firstItem;
+            
+            for (Node<T> i = firstItem; i != null; i = i.Next)
+                count++;
+
+            //Another way to achieve the same count as the above for educational purposes.    
+            // while (iter != null)
+            // {
+            //     count++;
+            //     iter = iter.Next;
+            // }
+            
+            return count;
+        }
     }
 }
--- test/data-structures-test/TestLinkedList.cs
+++ test/data-structures-test/TestLinkedList.cs
@@ -6,8 +6,38 @@ namespace data_structures
     public class TestLinkedList
     {
         [Fact]
-        public void Test1()
+        public void LinkedListCanAddItems()
         {
+            LinkedList<string> ll = new LinkedList<string>();
+            ll.AddFirst("one");
+            ll.AddFirst("two");
+            ll.AddFirst("three");
+            ll.AddFirst("four");
+            ll.AddFirst("five");
         }
+
+        [Fact]
+        public void LinkedListCountIsCorrectAfterAdding()
+        {
+            LinkedList<string> ll = new LinkedList<string>();
+            ll.AddFirst("one");
+            ll.AddFirst("two");
+            ll.AddFirst("three");
+            ll.AddFirst("four");
+            ll.AddFirst("five");
+
+            Assert.Equal(5, ll.count());
+        }
+
+                [Fact]
+        public void LinkedListCountIsCorrectBeforeAndAfterAddingOneItem()
+        {
+            LinkedList<string> ll = new LinkedList<string>();
+            Assert.Equal(0, ll.count());
+            ll.AddFirst("one");
+            Assert.Equal(1, ll.count());
+        }
+
+
     }
 }
